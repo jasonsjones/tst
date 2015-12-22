aed71886113db2679224377a835fa11bf267fb72
Jason Jones <jsjones96@gmail.com>
2015-12-22 15:28:14 +0000

    Implement get method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
     function TSTNode(value) {
         this.character = null;
+
         this.right = null;
         this.left = null;
         this.middle = null;
@@ -14,6 +15,26 @@
         this.root = null;
         this.size = 0;
     }
+
+    TST.prototype.get = function (key) {
+        var node = getNode(this.root, key, 0);
+        return node.value;
+
+        function getNode (node, key, idx) {
+            if (node === null) {
+                return null;
+            }
+            var c = key[idx];
+            if (c < node.character)
+                return getNode(node.left, key, idx);
+            else if (c > node.character)
+                return getNode(node.right, key, idx);
+            else if (idx < key.length -1)
+                return getNode(node.middle, key, idx + 1);
+            else
+                return node;
+        }
+    };
 
     TST.prototype.put = function (key, value) {
         var self = this;
@@ -40,5 +61,17 @@
     };
 
     module.exports = TST;
+
+    if (module.parent === null) {
+       main();
+    }
+
+    function main() {
+        console.log('running module...');
+        var tst = new TST();
+        tst.put("jason", 32);
+        tst.put("jones", 42);
+        console.log(tst.get("jones"));
+    }
 
 }());
