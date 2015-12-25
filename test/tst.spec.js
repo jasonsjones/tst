987b1219c7efa59c2d8e059fa3209f1e594e02e0
Jason Jones <jsjones96@gmail.com>
2015-12-25 14:56:50 +0000

    Add first test

--- test/tst.spec.js
+++ test/tst.spec.js
@@ -2,4 +2,8 @@ var expect = require('chai').expect;
 var TST = require('../');
 
 describe('Ternary Search Tree', function () {
+    it('exists when instantiated', function () {
+        var tst = new TST();
+        expect(tst).to.exist;
+    });
 });
